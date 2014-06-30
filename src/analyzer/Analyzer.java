b6560f2524cff9dbfc4ec265096752f55cfdc1fb
Sam Ervin <samuelthomaservin@gmail.com>
2014-06-29 19:49:11 -0500

    Analyzer now reads directly from csv file

--- src/analyzer/Analyzer.java
+++ src/analyzer/Analyzer.java
@@ -1,51 +1,61 @@
 package analyzer;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+//presently there is a bug where tweets that have newline characters in them really fuck up the algorithm.
+//I'll try and make a sanitizer but please just remove extra newlines for now
 public class Analyzer {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		String filename = "tweets.csv";
+		if(args.length > 0) {
+			filename = args[0];
+		}
 		
-		System.out.print("How many tweets are there? [number of lines in csv file - 1] ");
-		int numtweets = in.nextInt();
+		Scanner in = null;
+		try {
+			//for reasons I don't understand, the BufferedReader is necessary
+			//otherwise it cuts out really really quickly.
+			in = new Scanner(new BufferedReader(new FileReader(filename)));
+			in.nextLine();
+		} catch (FileNotFoundException e) {
+			System.out.println("tweets.csv must be in the same directory, or specified as an arg");
+			e.printStackTrace();
+		}
 		
+		int numtweets = 0;
+		ArrayList<Long> tweetids = new ArrayList<Long>(); //longs are currently big enough by 1 order
+		ArrayList<Long> replystatusids = new ArrayList<Long>(); //integers are just 1 order too small
+		ArrayList<Integer> replyuserids = new ArrayList<Integer>();
+		ArrayList<String> timestamps = new ArrayList<String>();
+		ArrayList<String> sources = new ArrayList<String>();
+		ArrayList<String> tweets = new ArrayList<String>();
+		ArrayList<Integer> rtstatusids = new ArrayList<Integer>();
+		ArrayList<Integer> rtuserids = new ArrayList<Integer>();
+		ArrayList<String> rttimestamps = new ArrayList<String>();
+		ArrayList<String> expandedurls = new ArrayList<String>();
 		
-		
-		System.out.println("Paste the 'in_reply_to_user_id' field:");
-		ArrayList<Integer> repliedids = new ArrayList<Integer>();
-		ArrayList<Integer> repliednum = new ArrayList<Integer>(); //maps are dumb you can't sort them
-		int numReplies = 0;
-		
-		for(int i = 0; i < numtweets; i++) {
-			String s = in.nextLine();
-			if(!s.equals("")) {
-				int id = Integer.parseInt(s);
-				numReplies++;
-				
-				if(repliedids.contains(id)) {
-					int index = repliedids.indexOf(id);
-					repliednum.set(index, repliednum.get(index) + 1);
-				} else {
-					repliedids.add(id);
-					repliednum.add(1);
-				}
+		while(in.hasNextLine()) {
+			String[] s = in.nextLine().split(",");
+			
+			long tweetid = Long.parseLong(s[0].substring(1, s[0].length()-1)); //cut out the quotes
+			tweetids.add(tweetid);
+			
+			String replystatusid = s[1].substring(1, s[1].length()-1); //cut out the quotes
+			if(replystatusid.length() > 0) {
+				replystatusids.add(Long.parseLong(replystatusid));
+			} else {
+				replystatusids.add((long) -1); //apparently -1 is not a long, java what are you doing
 			}
-		}
-		
-		bubbleSort(repliedids, repliednum); //easy > good
-		
-		int percReplies = (int) ((double) numReplies / (double) numtweets * 100);
-		System.out.println("Number of people replied to: " + repliedids.size());
-		System.out.println("Number of replies: " + numReplies);
-		System.out.println("Percentage of tweets that are replies: " + percReplies + "%");
-		for(int i = 0; i < repliedids.size(); i++) {
-			System.out.println(repliedids.get(i) + "\t\t" + repliednum.get(i));
-		}
-		
+		}	
 		
 		in.close();
+		System.out.println("numtweets: " + tweetids.size());
 	}
 	
 	public static void bubbleSort(ArrayList<Integer> ids, ArrayList<Integer> num) {
